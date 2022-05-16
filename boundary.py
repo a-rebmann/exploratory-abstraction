62adfa8a8fc96ee3596f4d04bfd62b75a101925e
Adrian Rebmann <rebmann@informatik.uni-mannheim.de>
2022-05-16 14:39:12 +0200

    fix

--- boundary.py
+++ boundary.py
@@ -35,7 +35,7 @@ result, loaded = reader.load_result(config)
 
 clr = XES_NAME_DF_NUM
 #px.figure(figsize=(30, 30), dpi=300)
-fig = px.scatter(result.pca["x"], result.pca["y"])
+fig = px.scatter(x=result.pca["x"].tolist(), y=result.pca["y"].tolist())
 
 # Plot!
 st.plotly_chart(fig, use_container_width=True)
